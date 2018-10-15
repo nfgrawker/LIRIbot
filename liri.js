996e5a997b7531312d1ee2ad0524205b33edc0e9
nfgrawker <nfgrawker@gmail.com>
2018-10-15 08:04:31 -0500

    added spotify function

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const keys  =  require("./keys.js")
 const fs = require("fs")
 const request = require("request")
-const spotify = require("node-spotify-api")
+const Spotify = require("node-spotify-api")
 const moment = require("moment")
 
 const arg1 = process.argv[2]
@@ -14,13 +14,36 @@ function concert(){
   request(url, function (error, response, body) {
   let information = JSON.parse(body)
   for (i in information){
-  var time = moment(information[i].datetime,"YYYY-MM-DDTHH:mm:ss").format("MM-DD-YYYY")
-  console.log("Venue Name: ",information[i].venue.name)
-  console.log("Location: ",information[i].venue.city,", ",information[i].venue.country)
-  console.log("Date: ",time)
-  console.log("----------------------")
+    var time = moment(information[i].datetime,"YYYY-MM-DDTHH:mm:ss").format("MM-DD-YYYY")
+    console.log("Venue Name: ",information[i].venue.name)
+    console.log("Location: ",information[i].venue.city,", ",information[i].venue.country)
+    console.log("Date: ",time)
+    console.log("----------------------")
+    }
+  });
 }
-});
+function spotifySearch(){
+  var song = arg2
+  var artists = ""
+  var spotify = new Spotify({
+  id: keys.keys.id,
+  secret: keys.keys.secret
+  });
+  spotify.search({ type: 'track', query: song }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+    }
+    for (i in data.tracks.items[0].artists){
+      artists += data.tracks.items[0].artists[i].name
+      if (i > 0 ){
+        artists += ", "
+      }
+    }
+    console.log("Artist(s) : ",artists)
+    console.log("Song Name : ",data.tracks.items[0].name)
+    console.log("Album : ", data.tracks.items[0].album.name)
+    console.log("Preview URL : ", data.tracks.items[0].external_urls.spotify)
+  })
 }
 
 
@@ -36,6 +59,6 @@ else if (arg1 == "movie-this") {
 else if (arg1 = "do-what-it-says") {
   text()
 }
-else(){
+else{
   console.log("did not understand your command")
 }
